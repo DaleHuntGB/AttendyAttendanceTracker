423b96ebcb1773c5801145df3985c71da1c337ed
Dale Hunt <dalehunt@gmail.com>
2023-02-24 21:46:10 +0000

    Check-In Page basically done. Beck - Leave my CSS alone. Original and edited images uploaded. Removed texturing.

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -4,7 +4,7 @@ function UpdateTimeOnOpen()
     let CurrentTime = new Date()
     let Hours = CurrentTime.getHours()
     let Minutes = CurrentTime.getMinutes()
-    
+
     // JavaScript doesn't know what to do with non-double digits
     if (Minutes < 10)
     {
@@ -22,14 +22,23 @@ function UpdateStudentName() // Demonstration Only!
 {
     let StudentFirstName = ["Beck", "Max", "Dale", "Artemi", "Lynx"]; 
     let NumOfStudents = StudentFirstName.length
-    let RandomizeStudent = StudentFirstName[Math.floor(Math.random()*NumOfStudents)];
+    let RandomizeStudent = StudentFirstName[Math.floor(Math.random() * NumOfStudents)];
     document.querySelector(".StudentFirstName").textContent = "Welcome " + RandomizeStudent;
+}
+
+function UpdateModule()
+{
+    let ModuleTitles = ["CI536: Integrated Group Project", "CI520: Narrative Game Design", "CI517: Game Engine Fundamentals"];
+    let NumOfModules = ModuleTitles.length
+    let RandomizeModules = ModuleTitles[Math.floor(Math.random() * NumOfModules)];
+    document.querySelector(".ModuleTitle").textContent = RandomizeModules;
 }
 
 function OnWindowLoad()
 {
     UpdateTimeOnOpen();
     UpdateStudentName()
+    UpdateModule();
 }
 
 window.onload = OnWindowLoad();
